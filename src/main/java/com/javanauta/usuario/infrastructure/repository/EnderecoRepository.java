ab08bb8de144d99decfc685c6b45d328d17a8333
Sabrina <sabrinavitreis@gmail.com>
2026-06-27 19:24:19 -0300

    Adicionadas classes de secutiry

---
+++ src/main/java/com/javanauta/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.javanauta.usuario.infrastructure.repository;
+
+import com.javanauta.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository <Endereco, Long> {
+}
---
+++ src/main/java/com/javanauta/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package com.javanauta.usuario.infrastructure.repository;
+
+
+import com.javanauta.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
